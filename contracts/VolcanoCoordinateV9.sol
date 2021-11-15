a0db3b298817ea457fc90470858dc563059108c1
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2021-11-14 20:26:20 -0500

    Update and rename VolcanoCoordinateV8.sol to VolcanoCoordinateV9.sol

--- contracts/VolcanoCoordinateV9.sol
+++ contracts/VolcanoCoordinateV9.sol
@@ -9,55 +9,39 @@ contract ERC20TokenContract is ERC20('Chainlink', 'LINK') {}
 contract VolcanoInsurance is ChainlinkClient {
     
     using Chainlink for Chainlink.Request;
-    address public ChainlinkTokenAddressRinkeby = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
-    ERC20TokenContract tokenObject = ERC20TokenContract(ChainlinkTokenAddressRinkeby);
 
-    // int public numberOfHits;
     int public Latitude;
     int public Longitude;
     int public Year;
     int public Month;
     int public Day;
     uint private fee;
-    address private oracle;
     bytes32 private jobId;
+    address private oracle;
+    address private ChainlinkTokenAddressRinkeby = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709;
+    ERC20TokenContract tokenObject = ERC20TokenContract(ChainlinkTokenAddressRinkeby);
 
     constructor() {
         setPublicChainlinkToken();
-        // jobId = "3b7ca0d48c7a4b2da9268456665d11ae"; //WORKING UINT
-        //oracle = 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40; //WORKING UINT
-        jobId = "e5b0e6aeab36405ba33aea12c6988ed6";  //WORKING INT FOR NEGATIVE VALUES
-        oracle = 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40; //WORKING INT FOR NEGATIVE VALUES
-        fee = 0.1 * 10 ** 18; // (Varies by network and job)
+        jobId = "e5b0e6aeab36405ba33aea12c6988ed6";  //WORKING INT FOR NEGATIVE VALUES          // jobId = "3b7ca0d48c7a4b2da9268456665d11ae"; //WORKING UINT
+        oracle = 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40; //WORKING INT FOR NEGATIVE VALUES         //oracle = 0x3A56aE4a2831C3d3514b5D7Af5578E45eBDb7a40; //WORKING UINT    
+        fee = 1*10**16; // (Varies by network and job)
     }
     
-    // function request_numberOfHits() public returns (bytes32 requestId) 
-    // {
-    //     Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_request_numberOfHits.selector);
-    //     request.add("get", "https://public.opendatasoft.com/api/records/1.0/search/?dataset=significant-volcanic-eruption-database&q=&refine.year=1727&refine.month=08&refine.day=03&refine.country=Iceland");
-    //     request.add("path", "nhits");
-    //     request.addInt("times", 10**18);
-    //     return sendChainlinkRequestTo(oracle, request, fee);
-    // }
-    // function fulfill_request_numberOfHits(bytes32 _requestId, int _numberOfHits) public recordChainlinkFulfillment(_requestId)
-    // {
-    //     numberOfHits = _numberOfHits;
-    // }
-    
     function request_All_Coordinate_Data() public {
-        require(tokenObject.balanceOf(address(this)) >= 2*(10*17), "CONTRACT NEEDS 0.2 LINK TO DO THIS! PLEASE SEND LINK!");
+        require(tokenObject.balanceOf(address(this)) >= 2*(10*16), "CONTRACT NEEDS 0.02 LINK TO DO THIS! PLEASE SEND LINK!");
         request_Latitude();
         request_Longitude();
     }
     
     function request_All_Time_Data() public {
-        require(tokenObject.balanceOf(address(this)) >= 3*(10*17), "CONTRACT NEEDS 0.3 LINK TO DO THIS! PLEASE SEND LINK!");
+        require(tokenObject.balanceOf(address(this)) >= 3*(10*16), "CONTRACT NEEDS 0.03 LINK TO DO THIS! PLEASE SEND LINK!");
         request_Year();
         request_Month();
         request_Day();
     }
     
-    function request_Latitude() public returns (bytes32 requestId) {
+    function request_Latitude() private returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_request_Latitude.selector);
         request.add("get", "https://public.opendatasoft.com/api/records/1.0/search/?dataset=significant-volcanic-eruption-database&q=&refine.year=1727&refine.month=08&refine.day=03&refine.country=Iceland");
         request.add("path", "records.0.fields.coordinates.0");
@@ -69,7 +53,7 @@ contract VolcanoInsurance is ChainlinkClient {
         Latitude = _Latitude;
     }
     
-    function request_Longitude() public returns (bytes32 requestId) {
+    function request_Longitude() private returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_request_Longitude.selector);
         request.add("get", "https://public.opendatasoft.com/api/records/1.0/search/?dataset=significant-volcanic-eruption-database&q=&refine.year=1727&refine.month=08&refine.day=03&refine.country=Iceland");
         request.add("path", "records.0.fields.coordinates.1");
@@ -81,7 +65,7 @@ contract VolcanoInsurance is ChainlinkClient {
         Longitude = _Longitude;
     }
     
-    function request_Year() public returns (bytes32 requestId) {
+    function request_Year() private returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_request_Year.selector);
         request.add("get", "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam");
         request.addInt("times", 10**18);
@@ -92,7 +76,7 @@ contract VolcanoInsurance is ChainlinkClient {
         Year = _Year; 
     }
     
-    function request_Month() public returns (bytes32 requestId) {
+    function request_Month() private returns (bytes32 requestId) {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_request_Month.selector);
         request.add("get", "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam");
         request.addInt("times", 10**18);
@@ -103,7 +87,7 @@ contract VolcanoInsurance is ChainlinkClient {
         Month = _Month; 
     }
     
-    function request_Day() public returns (bytes32 requestId)  {
+    function request_Day() private returns (bytes32 requestId)  {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill_request_Day.selector);
         request.add("get", "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam");
         request.addInt("times", 10**18);
