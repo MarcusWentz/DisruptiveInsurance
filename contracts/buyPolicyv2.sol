a83079d94bb681b537e23cdffadf3ff30ee76217
codelace <91913588+codelace@users.noreply.github.com>
2021-11-16 14:04:07 -0800

    Update buyPolicyv2.sol

--- contracts/buyPolicyv2.sol
+++ contracts/buyPolicyv2.sol
@@ -29,7 +29,7 @@ contract disruptiveInsurance {
         require(owner != msg.sender, "Error: Owner cannot self-insure"); // Policy purchaser must not be owner. 
         require(address(this).balance > 0, 'Error: Owner insufficient funds'); // Owner must have funds to cover policy purchase. Made >0 in case multiple policy purchases are made in the same contract for a given address (i.e owner will agree > 1 ETH).
         require(msg.value == (10 ** 18), 'Error: Please submit your request with insurance contribution of 0.001 Ether'); // Policy purchaser must be sending their share of insurance contract amount.
-        require(!policies[msg.sender].bought,"Error: You've already purchased insurance"); // Problem is that new addresses still cannot purchase. 
+        require(!policies[msg.sender].bought,"Error: You've already purchased insurance"); // Checks if requester has already bought insurance.  
         
         policySigner = msg.sender;
         policies[policySigner] = policy(Latitude, Longitude, true, ownerAmount);
