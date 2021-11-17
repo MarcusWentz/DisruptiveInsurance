3863398e2bb515f024e1b2856b708b5fcdb748c6
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2021-11-17 00:14:37 -0500

    Update setURLJSONandTimeCompressV3.sol

--- contracts/setURLJSONandTimeCompressV3.sol
+++ contracts/setURLJSONandTimeCompressV3.sol
@@ -15,6 +15,14 @@ contract buildStringOnContract {
         require(bytes(day).length == 2, "JSON must have day as 2 characters at all times!");
         urlRebuiltJSON= string( abi.encodePacked(askYear,year,askMonth,month,askDay,day,askCountry,country) );
     }
+    
+    function inputValuesRaw(string memory year, string memory month, string memory day, string memory country) public
+    {
+        require(bytes(month).length == 2, "JSON must have month as 2 characters at all times!");
+        require(bytes(day).length == 2, "JSON must have day as 2 characters at all times!");
+        urlRebuiltJSON= string( abi.encodePacked("https://public.opendatasoft.com/api/records/1.0/search/?dataset=significant-volcanic-eruption-database&q=&refine.year=",year,
+        "&refine.month=",month,"&refine.day=",day,"&refine.country=",country) );
+    }
 
     int public YearS = 2021;
     int public MonthS = 11;
