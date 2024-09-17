36192c17ab4877751a57027c2cd057eb9f83d50f
MarcusWentz <markzcollege404@gmail.com>
2024-09-16 21:38:54 -0400

    Axios Javascript test

---
+++ contracts/testChainlinkGetJsonAxios/timeApi.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+
+getJsonValues()
+
+async function getJsonValues() {
+
+    // let baseUrl = "http://127.0.0.1:8080/"
+	// let baseUrl = "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam";
+    // Removed "www." and the request still works.
+	let baseUrl = "https://timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam";
+    let responseRawJSON = await axios.get(baseUrl);
+    let responseDataJSON = responseRawJSON.data;
+    console.log(responseRawJSON)
+    console.log(responseDataJSON)
+    console.log(responseDataJSON.year)
+    console.log(responseDataJSON.day)
+    console.log(responseDataJSON.hour)
+}
