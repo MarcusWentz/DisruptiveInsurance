818f60166687b4bca0c4afb2d9ce8db64ad14fef
MarcusWentz <markzcollege404@gmail.com>
2024-09-16 20:50:38 -0400

    Correct jobId

--- contracts/src/gasTests/timeChainlinkOracleTest.sol
+++ contracts/src/gasTests/timeChainlinkOracleTest.sol
@@ -13,6 +13,8 @@ interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
 }
 
+error notEnoughLinkForThreeRequests();
+
 contract timeChainlinkOracleTest is ChainlinkClient {
         
     using Chainlink for Chainlink.Request;
@@ -28,7 +30,7 @@ contract timeChainlinkOracleTest is ChainlinkClient {
     
     uint256 public constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10; // 0.1 * 10**18 (0.1 LINK)
     address public constant chainlinkTokenAddressSepolia = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
-    string  private constant jobIdGetInt256Sepolia ="fcf4140d696d44b687012232948bdd5d"; 
+    string  private constant jobIdGetInt256Sepolia ="ca98366cc7314957b8c012c72f05aeeb"; 
     address private constant oracleSepolia = 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD; 
 
     constructor() {
@@ -37,8 +39,8 @@ contract timeChainlinkOracleTest is ChainlinkClient {
        
     // Oracle request time from JSON endpoint:
     function OracleRequestPresentTime() public {
-        uint256 requestPresentTimeLinkFee = IERC20(address(chainlinkTokenAddressSepolia)).balanceOf(address(this));
-        require(requestPresentTimeLinkFee >= 3*ORACLE_PAYMENT, "CONTRACT NEEDS 0.03 LINK TO DO THIS! PLEASE SEND LINK TO THIS CONTRACT!!");
+        uint256 requestPresentTimeLinkFee = IERC20(address(chainlinkTokenAddressSepolia)).balanceOf(address(this));    
+        if(requestPresentTimeLinkFee < 3*ORACLE_PAYMENT) revert notEnoughLinkForThreeRequests();
         // Chainlink requests.
         request_YearPresent();
         request_MonthPresent();
@@ -46,7 +48,8 @@ contract timeChainlinkOracleTest is ChainlinkClient {
     } 
 
     // Sepolia Gas:
-    // 
+    // Gas Limit & Usage by Txn:
+    // 432,297 | 279,463 (64.65%) 
     function request_YearPresent() public {
         Chainlink.Request memory req = _buildChainlinkRequest(
             stringToBytes32(jobIdGetInt256Sepolia),
