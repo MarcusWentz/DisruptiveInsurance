09e218003d5bab94574e5933e75b4c0175a4a2cc
MarcusWentz <52706599+MarcusWentz@users.noreply.github.com>
2024-09-18 16:17:45 -0400

    jobIdGetUint256Sepolia rename

--- contracts/src/gasTests/timeChainlinkOracleTest.sol
+++ contracts/src/gasTests/timeChainlinkOracleTest.sol
@@ -30,7 +30,7 @@ contract timeChainlinkOracleTest is ChainlinkClient {
     
     uint256 public constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10; // 0.1 * 10**18 (0.1 LINK)
     address public constant chainlinkTokenAddressSepolia = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
-    string  private constant jobIdGetInt256Sepolia ="ca98366cc7314957b8c012c72f05aeeb"; 
+    string  private constant jobIdGetUint256Sepolia ="ca98366cc7314957b8c012c72f05aeeb"; 
     address private constant oracleSepolia = 0x6090149792dAAeE9D1D568c9f9a6F6B46AA29eFD; 
 
     constructor() {
@@ -52,7 +52,7 @@ contract timeChainlinkOracleTest is ChainlinkClient {
 
     function request_YearPresent() public {
         Chainlink.Request memory req = _buildChainlinkRequest(
-            stringToBytes32(jobIdGetInt256Sepolia),
+            stringToBytes32(jobIdGetUint256Sepolia),
             address(this),
             this.fulfill_request_YearPresent.selector
         );
@@ -71,7 +71,7 @@ contract timeChainlinkOracleTest is ChainlinkClient {
   
     function request_MonthPresent() public {
         Chainlink.Request memory req = _buildChainlinkRequest(
-            stringToBytes32(jobIdGetInt256Sepolia),
+            stringToBytes32(jobIdGetUint256Sepolia),
             address(this),
             this.fulfill_request_MonthPresent.selector
         );
@@ -90,7 +90,7 @@ contract timeChainlinkOracleTest is ChainlinkClient {
 
     function request_DayPresent() public {
         Chainlink.Request memory req = _buildChainlinkRequest(
-            stringToBytes32(jobIdGetInt256Sepolia),
+            stringToBytes32(jobIdGetUint256Sepolia),
             address(this),
             this.fulfill_request_DayPresent.selector
         );
@@ -112,7 +112,7 @@ contract timeChainlinkOracleTest is ChainlinkClient {
     // 195,715 | 128,292 (65.55%)  
     function requestEthereumPrice() public {
         Chainlink.Request memory req = _buildChainlinkRequest(
-            stringToBytes32(jobIdGetInt256Sepolia),
+            stringToBytes32(jobIdGetUint256Sepolia),
             address(this),
             this.fulfillEthereumPrice.selector
         );
