b139aaaa94eba6d4cd5aa0a4d41d623bc1e1261b
MarcusWentz <markzcollege404@gmail.com>
2024-10-04 02:20:31 -0400

    abiEncodeDecodeHexSting

---
+++ contracts/src/gasTests/abiEncodeDecodeHexSting.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+// Example modified from:
+// https://solidity-by-example.org/abi-decode/
+
+contract abiEncodeDecodeHexSting {
+
+    function encode(
+        uint256 unixTime,
+        int256 lat,
+        int256 lon
+    ) external pure returns (bytes memory) {
+        return abi.encode(unixTime, lat, lon);
+    }
+
+    function decode(bytes calldata data)
+        external
+        pure
+        returns (
+            uint256 unixTime,
+            int256 lat,
+            int256 lon
+        )
+    {
+        // (uint x, address addr, uint[] memory arr, MyStruct myStruct) = ...
+        (unixTime, lat, lon) =
+            abi.decode(data, (uint256, int256, int256));
+    }
+}
