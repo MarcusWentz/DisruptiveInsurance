b5cb7d6af818eb9f2d8bbbc65772c87c850ade4c
MarcusWentz <markzcollege404@gmail.com>
2024-10-09 14:17:24 -0400

    Test fork Base Sepolia

---
+++ contracts/test/VolcanoInsurance.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import "forge-std/Test.sol";
+import {VolcanoInsurance, IVolcanoInsurance} from "../src/VolcanoInsurance.sol";
+
+contract VolcanoInsuranceTest is Test, IVolcanoInsurance { 
+
+    VolcanoInsurance public volcanoInsurance;
+
+    //Functions fallback and receive used when the test contract is sent msg.value to prevent the test from reverting.
+    fallback() external payable {}     // Fallback function is called when msg.data is not empty
+    receive() external payable {}      // Function to receive Ether. msg.data must be empty
+
+    function setUp() public {
+        volcanoInsurance = new VolcanoInsurance();
+        // assertEq(weigold.MAX_BPS(),10000);
+        // assertEq(weigold.SCALE_FEE(),30);
+        assertEq(volcanoInsurance.owner(),address(this));
+    }
+
+    function test_OracleEthUsdPrice() public {
+        assertEq(0,volcanoInsurance.openWeiToInsure());
+        volcanoInsurance.ownerSendOneEthToContractFromInsuranceBusiness{value: 1 ether}();
+        assertEq(1 ether,volcanoInsurance.openWeiToInsure());
+    }
+
+    // function testOracleEthGoldPrice() public {
+    //     uint256 priceEthGold = weigold.getLatestWeiGoldPrice();
+    //     assertGt(priceEthGold,0);
+    // }
+
+    // function testOwnerUpdateSlotsValid() public {
+    //     assertEq(weigold.vendingSlotCount(0),0);
+    //     weigold.ownerUpdateSlots(0,1);
+    //     assertEq(weigold.vendingSlotCount(0),1);
+    // }
+
+    // function testOwnerUpdateSlotsNotOwner() public {
+    //     vm.startPrank(address(0)); //Change the address to not be the owner. The owner is address(this) in this context.
+    //     // vm.expectRevert(NotOwner.selector);    //Revert if not the owner. Custom error from SimpleStorage.
+    //     vm.expectRevert("UNAUTHORIZED");    //Revert if not the owner. Custom error from SimpleStorage.
+    //     weigold.ownerUpdateSlots(0,1);
+    // }
+
+    // function testBuyGoldSlotEmpty() public {
+    //     vm.startPrank(address(0)); //Change the address to not be the owner. The owner is address(this) in this context.
+    //     assertEq(weigold.vendingSlotCount(0),0);
+    //     vm.expectRevert(SlotEmpty.selector);    //Revert if not the owner. Custom error from SimpleStorage.
+    //     weigold.buyGold(0);
+    // }
+
+    // function testBuyGoldMsgValueTooSmall() public {
+    //     testOwnerUpdateSlotsValid();
+    //     vm.startPrank(address(0)); //Change the address to not be the owner. The owner is address(this) in this context.
+    //     assertEq(weigold.vendingSlotCount(0),1);
+    //     vm.expectRevert(MsgValueTooSmall.selector);    //Revert if not the owner. Custom error from SimpleStorage.
+    //     weigold.buyGold(0);
+    // }
+
+    // function testBuyGoldValidWithPriceRefund() public {
+    //     testOwnerUpdateSlotsValid();
+    //     uint256 priceEthGold = weigold.getLatestWeiGoldPrice();
+    //     deal(address(0),2*priceEthGold);
+    //     vm.startPrank(address(0)); //Change the address to not be the owner. The owner is address(this) in this context.
+    //     uint256 prankBalanceBeforeRefund = address(0).balance;
+    //     assertEq(weigold.vendingSlotCount(0),1);
+    //     weigold.buyGold{value: priceEthGold + 1}(0);
+    //     assertEq(address(0).balance,prankBalanceBeforeRefund-priceEthGold);
+    // }
+
+    // function testBuyGoldValidWithNoPriceRefund() public {
+    //     testOwnerUpdateSlotsValid();
+    //     vm.startPrank(address(0)); //Change the address to not be the owner. The owner is address(this) in this context.
+    //     uint256 prankBalance = address(0).balance;
+    //     uint256 priceEthGold = weigold.getLatestWeiGoldPrice();
+    //     assertGt(prankBalance,priceEthGold);
+    //     assertEq(weigold.vendingSlotCount(0),1);
+    //     weigold.buyGold{value:priceEthGold}(0);
+    // }
+
+}
