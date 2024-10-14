06e13e2dbfe8d13c3ee44ab7874cd5eb6a886fdd
MarcusWentz <markzcollege404@gmail.com>
2024-10-13 20:39:10 -0400

    Fix input json

--- contracts/src/VolcanoInsurance.sol
+++ contracts/src/VolcanoInsurance.sol
@@ -196,14 +196,14 @@ contract VolcanoInsurance is FunctionsClient , Owned , IVolcanoInsurance {
     // JavaScript source code
 
     // // CHAINLINK FUNCTIONS JSON URL EXAMPLE WITH NO INPUT ARGUMENTS.
-    // string constant javascriptSourceCode = "const { ethers } = await import('npm:ethers@6.10.0'); const apiResponse = await Functions.makeHttpRequest({url: `https://userclub.opendatasoft.com/api/explore/v2.1/catalog/datasets/les-eruptions-volcaniques-dans-le-monde/records?limit=20&refine=country%3A%22Italy%22&refine=date%3A%222017%2F03%2F15%22`}); if (apiResponse.error) {console.error(apiResponse.error);throw Error('Request failed');} const { data } = apiResponse; console.log('API response data:'); const dateNow = data.results[0].date; console.log(dateNow); const timeUnix = Math.floor(new Date(dateNow).getTime() / 1000); console.log(timeUnix); const latRaw = data.results[0].coordinates.lat; console.log(latRaw); const latScaled = latRaw*100; console.log(latScaled); const lonRaw = data.results[0].coordinates.lon; console.log(lonRaw); const lonScaled = lonRaw*100; console.log(lonScaled); console.log('Ethers.js version: ', ethers.version); const abiCoder = ethers.AbiCoder.defaultAbiCoder(); const encodedAbiString = abiCoder.encode(['uint256', 'int16', 'int16'],[ 327456000 , 4620 , -12218 ]); console.log(encodedAbiString); console.log(ethers.getBytes(encodedAbiString)); return ethers.getBytes(encodedAbiString);";
-   
-    //CHAINLINK FUNCTIONS JSON URL EXAMPLE WITH 4 INPUT ARGUMENTS:
-    // const filterCountry = Italy;
-    // const filterYear = 2017;
-    // const filterMonth = 03;
-    // const filterDay = 15;
-    string constant javascriptSourceCode = "const { ethers } = await import('npm:ethers@6.10.0'); const filterCountry = args[0]; const filterYear = args[1]; const filterMonth = args[2]; const filterDay = args[3]; console.log( filterCountry , filterYear, filterMonth, filterDay); const jsonUrlStringBase = 'https://userclub.opendatasoft.com/api/explore/v2.1/catalog/datasets/les-eruptions-volcaniques-dans-le-monde/records?limit=20&refine=country%3A%22'; const jsonUrlStringFilter = jsonUrlStringBase + filterCountry + '%22&refine=date%3A%22' + filterYear + '%2F' + filterMonth + '%2F' + filterDay + '%22'; console.log(jsonUrlStringFilter); const apiResponse = await Functions.makeHttpRequest({url: `https://userclub.opendatasoft.com/api/explore/v2.1/catalog/datasets/les-eruptions-volcaniques-dans-le-monde/records?limit=20&refine=country%3A%22Italy%22&refine=date%3A%222017%2F03%2F15%22`}); if (apiResponse.error) {console.error(apiResponse.error);throw Error('Request failed');} const { data } = apiResponse; console.log('API response data:'); const dateNow = data.results[0].date; console.log(dateNow); const timeUnix = Math.floor(new Date(dateNow).getTime() / 1000); console.log(timeUnix); const latRaw = data.results[0].coordinates.lat; console.log(latRaw); const latScaled = latRaw*100; console.log(latScaled); const lonRaw = data.results[0].coordinates.lon; console.log(lonRaw); const lonScaled = lonRaw*100; console.log(lonScaled); console.log('Ethers.js version: ', ethers.version); const abiCoder = ethers.AbiCoder.defaultAbiCoder(); const encodedAbiString = abiCoder.encode(['uint256', 'int16', 'int16'],[ 327456000 , 4620 , -12218 ]); console.log(encodedAbiString); console.log(ethers.getBytes(encodedAbiString)); return ethers.getBytes(encodedAbiString);";
+    // string constant javascriptSourceCode = "";
+
+    // //CHAINLINK FUNCTIONS JSON URL EXAMPLE WITH 4 INPUT ARGUMENTS:
+    // // const filterCountry = Italy;
+    // // const filterYear = 2017;
+    // // const filterMonth = 03;
+    // // const filterDay = 15;
+    // string constant javascriptSourceCode = "";
 
     constructor() FunctionsClient(routerBaseSepolia) Owned(msg.sender) {}
 
